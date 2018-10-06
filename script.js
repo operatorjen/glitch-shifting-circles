0953fbf364ac6739e088e365a415c3ec992075c6
Glitch (hello-webpage) <none>
2018-10-06 18:22:18 +0000

    👶🏜 Checkpoint ./script.js:378534/152 ./style.css:378534/346

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ const wrapper = document.querySelector('#wrapper')
 const overlay = document.querySelector('#overlay')
 
 let opacity = 0.8
-let r = 255
+let r = 55
 let g = 255
 let b = 255
 let a = 1.0
 
-for (let i = 0; i < 300; i++) {
+for (let i = 0; i < 500; i++) {
   const div = document.createElement('div')
   div.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`
   wrapper.appendChild(div)
   
   if (r < 1) {
-    r = 255 
+    r = 155 
   }
   
   if (g < 1) {
@@ -21,19 +21,28 @@ for (let i = 0; i < 300; i++) {
   }
   
   if (b < 1) {
-    b = 255
+    b = 155
   }
   
-  r -= 1
-  g -= 3
-  b -= 1
+  r -= 5
+  
+  if (r < 230) {
+    g -= 2
+    b -= 1
+  }
+  
+  a -= 0.01
+  
+  if (a < 0.1) {
+    a = 1.0
+  }
 }
 
-r = 255
+r = 215
 g = 20
 b = 250
 
-for (let i = 0; i < 300; i++) {
+for (let i = 0; i < 100; i++) {
   const div = document.createElement('div')
   div.style.opacity = opacity
   overlay.appendChild(div)
