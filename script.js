a51b0c8dab3d53748d93c097c5f898fbfed11730
Glitch (hello-webpage) <none>
2018-10-06 18:12:06 +0000

    🎽🌓 Checkpoint ./script.js:378534/746 ./style.css:378534/2

--- script.js
+++ script.js
@@ -1,45 +1,63 @@
 const wrapper = document.querySelector('#wrapper')
 const overlay = document.querySelector('#overlay')
 
-let opacity = 1.0
+let opacity = 0.8
 let r = 255
 let g = 255
 let b = 255
 let a = 1.0
 
-for (let i = 0; i < 255; i++) {
+for (let i = 0; i < 300; i++) {
   const div = document.createElement('div')
   div.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`
   wrapper.appendChild(div)
   
   if (r < 1) {
     r = 255 
-    g -= 1
-    b -= 1
   }
   
   if (g < 1) {
     g = 255 
-    b -= 1
-    r -= 1
   }
   
   if (b < 1) {
     b = 255
-    g -= 1
   }
   
   r -= 1
+  g -= 3
+  b -= 1
 }
 
-for (let i = 0; i < 255; i++) {
+r = 255
+g = 20
+b = 250
+
+for (let i = 0; i < 300; i++) {
   const div = document.createElement('div')
   div.style.opacity = opacity
   overlay.appendChild(div)
   
-  div.style.backgroundColor = `rgba`
-  opacity -= 0.01
+  r -= 1
+  g -= 3
+  b -= 5
+  
+  if (r < 1) {
+    r = 155
+  }
+  
+  if (g < 1) {  
+    g = 100
+  }
+  
+  if (b < 1) {
+    b = 150
+  }
+
+  
+  div.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`
+  opacity -= 0.05
   if (opacity < 0.05) {
-    opacity = 1.0 
+    opacity = 0.8
   }
 }
